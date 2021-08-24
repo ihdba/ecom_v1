262039838b89f0ff6269bb7e90e6d28bb19e5794
Ioannis Hatzidamianos <ihdba@yahoo.com>
2021-08-24 13:46:35 +0200

    I Loading is done

--- deli_vue/src/views/Product.vue
+++ deli_vue/src/views/Product.vue
@@ -32,6 +32,7 @@
 
 <script>
 import axios from 'axios'
+import { toast } from 'bulma-toast'
 
 export default {
     name: 'Product',
@@ -45,11 +46,13 @@ export default {
         this.getProduct() 
     },
     methods: {
-        getProduct() {
+        async getProduct() {
+            this.$store.commit('setIsLoading', true)
+
             const category_slug = this.$route.params.category_slug
             const product_slug = this.$route.params.product_slug
 
-            axios
+            await axios
                 .get(`/api/v1/products/${category_slug}/${product_slug}`)
                 .then(response => {
                     this.product = response.data
@@ -57,6 +60,8 @@ export default {
                 .catch(error =>{
                     console.log(error)
                 })
+
+            this.$store.commit('setIsLoading', false)
         },
         addToCart() {
             if (isNaN(this.quantity) || this.quantity < 1) {
@@ -69,6 +74,14 @@ export default {
             }
 
             this.$store.commit('addToCart', item)
+
+            toast({
+                message: 'The product was added to the cart',
+                type: 'is-success',
+                pauseOnHover: true,
+                duration: 2000,
+                position: 'bottom-right',
+            })
         }
     }
 }
